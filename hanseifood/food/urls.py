a408d42dd36261040fb8a48cb387d65091b8edcb
강혜미 <eham1003@naver.com>
2023-07-03 17:37:03 +0900

    식단표 api - /student

--- hanseifood/food/urls.py
+++ hanseifood/food/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.student_food_table, name="student_food_table")
+    path("student", views.student_food_table, name="student_food_table"),
+    path("", views.index, name="student_food_table")
 ]
--- hanseifood/food/views.py
+++ hanseifood/food/views.py
@@ -1,4 +1,3 @@
-
 from django.http import HttpResponse, JsonResponse
 from modules import excelParser
 
@@ -6,8 +5,10 @@ from modules import excelParser
 def index(request):
     return HttpResponse("Hello world!")
 
+
+# /student
 def student_food_table(request):
-    file_path = "modules/test1.xlsx"
+    file_path = "modules/test1.xlsx"  # 크롤링한 엑셀파일
     data = excelParser.parse_students_menu(file_path)
     response = JsonResponse(data, json_dumps_params={'ensure_ascii': False})
     return response
