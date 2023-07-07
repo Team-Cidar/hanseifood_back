6048f9dfa19d41abf3d394b45f3d55f2cfe66460
Park-SeungWoo <tmd24710@naver.com>
2023-07-07 16:02:05 +0900

    Feat: model 구성 완료

--- hanseifood/food/admin.py
+++ hanseifood/food/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Day, DayMeal, Meal
 
 # Register your models here.
+admin.site.register(Day)
+admin.site.register(DayMeal)
+admin.site.register(Meal)
--- hanseifood/food/models.py
+++ hanseifood/food/models.py
@@ -2,8 +2,23 @@ from django.db import models
 
 
 # Create your models here.
-class Days(models.Model):
+class Day(models.Model):
     date = models.DateField(null=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Meal(models.Model):
+    meal_name = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class DayMeal(models.Model):
+    day_id = models.ForeignKey(Day, on_delete=models.CASCADE)
+    meal_id = models.ForeignKey(Meal, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.title
