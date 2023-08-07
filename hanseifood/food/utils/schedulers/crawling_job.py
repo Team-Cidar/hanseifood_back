4fcd7d34b96fc639b9790b4681c65e1ad710fbec
Hoooooou-Jun <jipkim2@gmail.com>
2023-08-07 17:51:15 +0900

    requirement.txt 이동, 도카파일 수정

--- hanseifood/food/utils/schedulers/crawling_job.py
+++ hanseifood/food/utils/schedulers/crawling_job.py
@@ -28,7 +28,7 @@ def save_menu_scheduler_job():
                 return
 
         # crawling
-        crawler = MenuCrawler('../../../../chrome/chromedriver')
+        crawler = MenuCrawler('drivers/chromedriver')
         file_name = crawler.crawl()
 
         # parse
