011f43a6f1dfd525ba8f90bdcddbb6f691672b30
Hoooooou-Jun <jipkim2@gmail.com>
2023-08-30 17:34:20 +0900

    http CORS 수정

--- hanseifood/hanseifood/settings.py
+++ hanseifood/hanseifood/settings.py
@@ -214,7 +214,7 @@ LOGGING = {
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
-    'http://218.239.156.31:80',
+    'http://218.239.156.31',
     'http://localhost:8080',
     'http://localhost:80',
 ]
