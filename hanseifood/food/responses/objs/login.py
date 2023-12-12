89cb5939c717e2b783a0039d97f6480f247b6721
LSH <0867sh@naver.com>
2023-12-12 20:24:18 +0900

    로그인 기능 구현

--- hanseifood/food/responses/objs/login.py
+++ hanseifood/food/responses/objs/login.py
@@ -1,6 +1,6 @@
 from .abstract_model import AbstractModel
 
-class LoginModel(AbstractModel):
+class UserModel(AbstractModel):
     def __init__(self, user_id: str, user_nickname: str, is_exists: bool, customnickname: str, access_token: str):
         self.user_id = user_id
         self.user_nickname = user_nickname
@@ -15,5 +15,5 @@ class LoginModel(AbstractModel):
             "kakaonickname": self.user_nickname,
             "isExists": str(self.is_exists).lower(), # Convert bool to string
             "customnickname": self.customnickname,
-            "access_token" : self.access_token
+            "access_token": self.access_token
         }
