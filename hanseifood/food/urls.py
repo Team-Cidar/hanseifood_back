f257b3105cf24662d364286f5859d0574d406b03
LSH <0867sh@naver.com>
2023-12-12 22:13:43 +0900

    로그인 기능 구현

--- hanseifood/food/urls.py
+++ hanseifood/food/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView, TokenVerifyView,
 )
-
+from .core.jwt import jwt
 
 urlpatterns = [
     # /views/base_views
@@ -20,7 +20,7 @@ urlpatterns = [
     # path("verifyuser", login_views.verify_user, name = 'code'),
     # path("logout", login_views.logout, name = 'logout'),
     path("nickname", login_views.set_nickname, name ="set_nickname"),
-    path('api/token', login_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token', jwt.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     # /views/ticket_views
