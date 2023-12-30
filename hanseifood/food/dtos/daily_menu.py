9b56ec1547d85dd56282b8f887688b1345449b82
Park-SeungWoo <tmd24710@naver.com>
2023-12-30 18:50:08 +0900

    fix: fix py versino related grammar issue in DailyMenuDto's type

--- hanseifood/food/dtos/daily_menu.py
+++ hanseifood/food/dtos/daily_menu.py
@@ -1,12 +1,12 @@
-from typing import List, Optional
+from typing import List, Union
 from datetime import datetime
 
 from django.db.models import Model
 
 
 class DailyMenuDto:
-    def __init__(self, date: Optional[datetime, Model], student: list, employee: list, additional: list):
-        self.date: Optional[datetime, Model] = date
+    def __init__(self, date: Union[datetime, Model], student: list, employee: list, additional: list):
+        self.date: Union[datetime, Model] = date
         self.student: List[str] = student
         self.employee: List[str] = employee
         self.additional: List[str] = additional
