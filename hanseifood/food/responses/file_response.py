a775771547eb7e775c55e67ebced9218cf93d4a3
Park-SeungWoo <tmd24710@naver.com>
2024-01-04 21:28:24 +0900

    fix: file response content-disposition setting

--- hanseifood/food/responses/file_response.py
+++ hanseifood/food/responses/file_response.py
@@ -8,5 +8,5 @@ class FileResponse(AbstractResponse):
     def response(file_path: str, content_type: str, status_code: int = 200) -> HttpResponse:
         with open(file_path, 'rb') as file:
             response = HttpResponse(content=file.read(), content_type=content_type)
-            response['Content-Disposition'] = f'attachment; filename={file_path.split("/")[1]}'
+            response.headers['Content-Disposition'] = f'attachment; filename={file_path.split("/")[1]}'
             return response
