5a4b5958fceb23734ecfc30e9aa74195a48f49e2
Park-SeungWoo <tmd24710@naver.com>
2024-01-04 18:27:12 +0900

    fix: separate third-party api usage as class

---
+++ hanseifood/food/core/apis/kakao.py
@@ -0,0 +1,43 @@
+import requests
+
+from ..constants.strings import env_strings as env
+
+
+class KakaoApi:
+    @classmethod
+    def request_kakao_token(cls, authorization_code: str) -> str:
+        headers: dict = {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+        body: dict = {
+            'grant_type': 'authorization_code',
+            'client_id': env.KAKAO_REST_API_KEY,
+            'redirect_uri': env.KAKAO_REDIRECT_URI,
+            'code': authorization_code
+        }
+
+        token_response = requests.post(env.KAKAO_TOKEN_API_URL, headers=headers, data=body)
+        token_data: dict = token_response.json()
+        # refresh_token = token_data['refresh_token']  # kakao refresh token 필요시 사용
+        return token_data["access_token"]
+
+    @classmethod
+    def request_kakao_user_info(cls, kakao_access_token: str) -> dict:
+        headers: dict = {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+            'Authorization': f'Bearer {kakao_access_token}'
+        }
+
+        user_info_response = requests.post(env.KAKAO_USERINFO_API_URL, headers=headers)
+
+        user_data: dict = user_info_response.json()
+        user_properties: dict = user_data['properties']
+        kakao_id: str = str(user_data['id'])
+        kakao_nickname: str = user_properties['nickname']
+
+        return {
+            'kakao_id': kakao_id,
+            'kakao_nickname': kakao_nickname,
+            'kakao_email': ''  # email 동의 항목 추가 이후 함께 return 해야함
+        }
+
--- hanseifood/food/services/login_service.py
+++ hanseifood/food/services/login_service.py
@@ -1,10 +1,8 @@
-import requests
-
 from .abstract_service import AbstractService
+from ..core.apis.kakao import KakaoApi
+from ..core.utils.jwt_utils import get_token
 from ..enums.role_enums import UserRole
 from ..models import User
-from ..core.constants.strings import env_strings as env
-from ..core.utils.jwt_utils import get_token
 from ..responses.objs.login import UserLoginModel
 from ..repositories.user_respository import UserRepository
 
@@ -15,38 +13,21 @@ class LoginService(AbstractService):
 
     def do_login(self, code: str) -> UserLoginModel:
         # for kakao OAuth2
-        header_reusable: dict = dict()
-        body: dict = dict()
+        kakao_access_token: str = KakaoApi.request_kakao_token(code)
 
-        header_reusable['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8'
-        body['grant_type'] = 'authorization_code'
-        body['client_id'] = env.KAKAO_REST_API_KEY
-        body['redirect_uri'] = env.KAKAO_REDIRECT_URI
-        body['code'] = code
-
-        token_response = requests.post(env.KAKAO_TOKEN_API_URL, headers=header_reusable, data=body)
-        token_data: dict = token_response.json()
-
-        header_reusable['Authorization'] = f'Bearer ${token_data["access_token"]}'
-
-        user_info_response = requests.post(env.KAKAO_USERINFO_API_URL, headers=header_reusable)
-
-        user_data: dict = user_info_response.json()
-        user_properties: dict = user_data['properties']
-        kakao_id: str = str(user_data['id'])
-        kakao_nickname: str = user_properties['nickname']
+        kakao_user_info: dict = KakaoApi.request_kakao_user_info(kakao_access_token)
 
         # check user exists
-        exist, user_models = self.__user_repository.existsByKakaoId(kakao_id=kakao_id)
+        exist, user_models = self.__user_repository.existsByKakaoId(kakao_id=kakao_user_info['kakao_id'])
 
         if exist:
             return self._allow_login(user_models[0])
 
         return UserLoginModel(
             status=False,
-            kakao_id=kakao_id,
-            email='',
-            kakao_name=kakao_nickname
+            kakao_id=kakao_user_info['kakao_id'],
+            email=kakao_user_info['kakao_email'],
+            kakao_name=kakao_user_info['kakao_nickname']
         )
 
     def create_user(self, data: tuple) -> UserLoginModel:
