309e992d7bcb8e5d3e1ff8051dde41df248e9dd6
Park-SeungWoo <tmd24710@naver.com>
2024-02-05 18:29:06 +0900

    fix: add bind host to cors allowed host for dev

--- hanseifood/config/settings/dev.py
+++ hanseifood/config/settings/dev.py
@@ -1,7 +1,14 @@
 from ._base import *
+import socket
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 WSGI_APPLICATION = 'config.wsgi.dev.application'
 print('Development mode!')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+CORS_ALLOWED_ORIGINS.append(f"http://{s.getsockname()[0]}:8080")  # bind host of localhost
+s.close()
+print(CORS_ALLOWED_ORIGINS[-1])
