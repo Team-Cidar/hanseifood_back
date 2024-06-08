1ec6b25e895e9283771fe5537c48fc9aff9a45f7
Hoooooou-Jun <jipkim2@gmail.com>
2024-06-08 11:29:21 +0900

    allowed host에 reverse proxy container host 추가

--- hanseifood/config/settings/prod.py
+++ hanseifood/config/settings/prod.py
@@ -2,6 +2,6 @@ from ._base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', 'hanseiweeklymenu.me', 'www.hanseiweeklymenu.me', '218.239.156.31']
+ALLOWED_HOSTS = ['localhost', 'hanseiweeklymenu.me', 'www.hanseiweeklymenu.me', '218.239.156.31', 'reverse_proxy']
 WSGI_APPLICATION = 'config.wsgi.prod.application'
 print("Production mode!")
